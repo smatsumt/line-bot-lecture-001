02d8798afabe2548612421a4134b540925a0ca0b
Shinji Matsumoto <shinji.mtsmt@gmail.com>
2020-08-22 14:39:50 +0900

    オウム返しの最小限のリソースに限定

--- src/lambda1/lambda_handler.py
+++ src/lambda1/lambda_handler.py
@@ -5,40 +5,44 @@
 
 """
 
-import json
-import logging
 import os
-import traceback
+import json
 
-import line_handler
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
-logger = logging.getLogger(__name__)
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN', 'YOUR_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET', 'YOUR_CHANNEL_SECRET'))
 
 
 def lambda_handler(event, context):
-    _lambda_logging_init()
     headers = event["headers"]  # or event["multiValueHeaders"]
-    # prm1 = event["pathParameters"]["prm1"]
-    # prm2 = event["queryStringParameters"]["prm2"]
     body = event["body"]
 
-    logger.info(headers)
-    try:
-        result = line_handler.callback(headers, body)
-        return {"statusCode": 200, "body": json.dumps(result)}
-    except Exception as e:
-        result = {"result": "failed", "message": traceback.format_exc()}
-        logger.info(json.dumps(result), exc_info=e)
-        return {"statusCode": 500, "body": json.dumps(result)}
+    result = callback(headers, body)
+    return {"statusCode": 200, "body": json.dumps(result)}
 
 
-def _lambda_logging_init():
-    """
-    logging の初期化。LOGGING_LEVEL, LOGGING_LEVELS 環境変数を見て、ログレベルを設定する。
-      LOGGING_LEVELS - "module1=DEBUG,module2=INFO" という形の文字列を想定。自分のモジュールのみ DEBUG にするときなどに利用
-    """
-    logging.getLogger().setLevel(os.getenv('LOGGING_LEVEL', 'INFO'))  # lambda の場合はロガー設定済みのためこちらが必要
-    if os.getenv('LOGGING_LEVELS'):
-        for mod_lvl in os.getenv('LOGGING_LEVELS').split(','):
-            mod, lvl = mod_lvl.split('=')
-            logging.getLogger(mod.strip()).setLevel(lvl.strip())
+def callback(headers, body):
+    # get X-Line-Signature header value
+    signature = headers['X-Line-Signature']
+
+    # get request body as text
+    print("Request body: " + body)
+
+    # handle webhook body
+    handler.handle(body, signature)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    """ TextMessage handler """
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
