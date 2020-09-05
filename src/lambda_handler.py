d43ba71496ae8277c3cfa3dc5604f01d584bfbaf
Shinji Matsumoto <shinji.mtsmt@gmail.com>
2020-09-05 10:14:50 +0900

    さらにシンプル化

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 """
 オウム返し Line Bot
 """
 
 import os
-import json
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -15,26 +11,21 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN', 'YOUR_CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET', 'YOUR_CHANNEL_SECRET'))
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 
 def lambda_handler(event, context):
     headers = event["headers"]  # or event["multiValueHeaders"]
     body = event["body"]
 
-    result = callback(headers, body)
-    return {"statusCode": 200, "body": json.dumps(result)}
-
-
-def callback(headers, body):
     # get X-Line-Signature header value
     signature = headers['X-Line-Signature']
 
     # handle webhook body
     handler.handle(body, signature)
 
-    return 'OK'
+    return {"statusCode": 200, "body": "OK"}
 
 
 @handler.add(MessageEvent, message=TextMessage)
