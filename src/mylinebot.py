9f16f40f4e31ec0cf4038c2b3e96481a60502f0a
Shinji Matsumoto <shinji.mtsmt@gmail.com>
2020-10-05 22:36:39 +0900

    画像読み込み、Rekognition 呼び出しを追加

--- src/mylinebot.py
+++ src/mylinebot.py
@@ -4,15 +4,19 @@
 
 import os
 
+import boto3
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    ImageMessage,
 )
 
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+
+rekognition = boto3.client("rekognition")
 
 
 def lambda_handler(event, context):
@@ -37,3 +41,18 @@ def handle_text_message(event):
         event.reply_token,
         TextSendMessage(text=input_text))
 
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+    """ TextMessage handler """
+    # 画像を取得
+    message_content = line_bot_api.get_message_content(event.message.id)
+    sent_image_bytes = message_content.content
+
+    # Rekognition 呼び出し
+    result = rekognition.detect_faces(Image={"Bytes": sent_image_bytes}, Attributes=["ALL"])
+
+    # 結果を出す
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=str(result)))
